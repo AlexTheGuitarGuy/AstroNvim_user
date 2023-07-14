43d8758c66549f29dbda4bb09c5c49454a2f3ba2
Alex <andutzu.268@gmail.com>
2023-07-14 16:32:22 +0300

    added treesitter ensure_installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -4,9 +4,37 @@ return {
     auto_install = true,
   },
 
-config= function()
-require 'nvim-treesitter.install'.compilers = { 'zig' }
-end,
+  config = function()
+    require("nvim-treesitter.install").compilers = { "zig" }
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        "bash",
+        "c",
+        "c_sharp",
+        "cmake",
+        "cpp",
+        "css",
+        "dockerfile",
+        "git_config",
+        "gitignore",
+        "graphql",
+        "html",
+        "javascript",
+        "json",
+        "jsonc",
+        "llvm",
+        "lua",
+        "make",
+        "python",
+        "sql",
+        "svelte",
+        "todotxt",
+        "typescript",
+        "yaml",
+        "rust",
+      },
+    }
+  end,
   dependencies = {
     {
       "mrjones2014/nvim-ts-rainbow",
